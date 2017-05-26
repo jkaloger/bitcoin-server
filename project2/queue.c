34bc20a50b9a4e17e685466a006245073a9b2218
jkaloger <jkaloger@c796bed8-1823-4e2d-81b8-cf8d475cb5c1>
2017-05-26 02:43:01 +0000

    WORK

--- project2/queue.c
+++ project2/queue.c
@@ -6,14 +6,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-void enqueue(Queue *q, uint32_t diff, BYTE seed[64], uint64_t start, uint8_t worker_count)
+void enqueue(Queue *q, int sockfd, char *diff_stream, char *seed_stream, char *start_stream, char *worker_stream)
 {
     Queue new = malloc(sizeof(struct queue_t));
     Work w = malloc(sizeof(struct work_t));
-    w->difficulty = diff;
-    memcpy(w->seed, &seed, 64);
-    w->start = start;
-    w->worker_count = worker_count;
+    w->sockfd = sockfd;
+    bzero(w->difficulty, 9);
+    memcpy(w->difficulty, diff_stream, 8);
+    bzero(w->seed, 65);
+    memcpy(w->seed, seed_stream, 64);
+    bzero(w->start, 17);
+    memcpy(w->start, start_stream, 16);
+    bzero(w->worker_count, 3);
+    memcpy(w->worker_count, worker_stream, 2);
     new->work = w;
     new->next = NULL;
 
